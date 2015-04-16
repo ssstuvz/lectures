b7bc9ee9d571cce85cf86a821d71a107ddbd35da
Sergey Rykovanov <serge.rykovanov@gmail.com>
2015-04-16 20:36:27 +0200

    new version

--- generate_html.py
+++ generate_html.py
@@ -4,5 +4,5 @@ import os
 print "Generating the html files"
 
 os.system("ipython nbconvert index.ipynb")
-os.system("ipython nbconvert Cheat_sheat.ipynb")
+os.system("ipython nbconvert Cheat_sheet.ipynb")
 os.system("ipython nbconvert Charged_particles.ipynb")
